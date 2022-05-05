795564b264ce50e20aa1c8108b5fb974eb12cda6
Jose Lemuz <lemuz.jose@gmail.com>
2022-05-05 12:00:06 -0700

    establish mysql connection and queries

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const express= require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
   app.get('/', (req, res) => {
     res.json({
